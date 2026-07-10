f53a4e02ef2e9f800b02971288d6a90c3393177c
paida30 <paidamapurisa28@gmail.com>
2026-07-10 14:31:12 +0530

    Add files via upload

---
+++ infom.java
@@ -0,0 +1,24 @@
+//dsfcsdfdas
+
+import java.util.Scanner;
+
+class infom
+ {
+   public static void main ( String args[])
+   {
+     Scanner scn = new Scanner(System.in);
+
+     System.out.println("enter a string");
+	 String name = scn.nextLine();
+	 
+	 System.out.println("enter an integer");
+	 int num =  scn.nextInt();
+	 
+	
+     System.out.println("You entered a string: " +name);
+     System.out.println("You entered an integer: " +num);
+	 
+	 
+	 
+   }
+}
