537151a415ee65637443977576379e2a93c67900
paida30 <paidamapurisa28@gmail.com>
2026-07-10 14:31:43 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,7 @@
+class intro
+ {
+   public static void main( String args[])
+   {
+     System.out.println("hello \n myself paida\ni am in2nd year\ni am an international student\ni like to eat\ni am a sportswoman\nhow do you do\ni miss home\nthank you!");
+   }
+}
